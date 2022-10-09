7789a87498d5236dbddd6fdaf06cbd2d3ef70043
Kirinari <62498402+Kirinari@users.noreply.github.com>
2022-10-09 21:57:38 +0300

    Update test.py

--- 04/test.py
+++ 04/test.py
@@ -1,13 +1,16 @@
+"""
+hello pylint
+"""
 from customlist import CustomList
-a = CustomList([5, 1, 3, 7])
-b = CustomList([1, 2, 7])
-assert a - b == CustomList([4, -1, -4, 7])
-assert list(a - b) == [4, -1, -4, 7]
-assert a + b == CustomList([6, 3, 10, 7])
-assert list(a + b) == [6, 3, 10, 7]
-assert a + CustomList() == a
-assert list(a + CustomList()) == list(a)
+TEST_A = CustomList([5, 1, 3, 7])
+TEST_B = CustomList([1, 2, 7])
+assert TEST_A - TEST_B == CustomList([4, -1, -4, 7])
+assert list(TEST_A - TEST_B) == [4, -1, -4, 7]
+assert TEST_A + TEST_B == CustomList([6, 3, 10, 7])
+assert list(TEST_A + TEST_B) == [6, 3, 10, 7]
+assert TEST_A + CustomList() == TEST_A
+assert list(TEST_A + CustomList()) == list(TEST_A)
 assert [1, 2, 3] + CustomList([1, 1, 1]) == CustomList([9])
 assert list([1, 2, 3] + CustomList([1, 1, 1])) == [2, 3, 4]
 assert list(CustomList([1]) + [1, 2, 3]) == [2, 2, 3]
-assert str(a) == '5 1 3 7 \nsum: 16'
+assert str(TEST_A) == '5 1 3 7 \nsum: 16'
