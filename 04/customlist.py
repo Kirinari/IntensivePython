7f2cb05da8ba5fd1d5eb280d41bb7bf5dc03f8a0
Kirinari <62498402+Kirinari@users.noreply.github.com>
2022-10-09 21:35:07 +0300

    Update customlist.py

--- 04/customlist.py
+++ 04/customlist.py
@@ -1,17 +1,19 @@
-class MyList(list):
+class CustomList(list):
     def __init__(self, *argc, **kwargs):
         super().__init__(*argc, **kwargs)
 
     def __sub__(self, other):
-        if isinstance(other, list) or isinstance(other, MyList):
+        if isinstance(other, list) or isinstance(other, CustomList):
             len_self = len(self)
             len_other = len(other)
             result = [0] * max(len_self, len_other)
+
             for i in range(len_self):
                 result[i] = result[i] + self[i]
             for i in range(len_other):
                 result[i] = result[i] - other[i]
-            return result
+            return CustomList(result)
+
         else:
             raise TypeError(f"unsupported operand type(s) for -")
 
@@ -24,12 +26,12 @@ class MyList(list):
                 result[i] = result[i] - self[i]
             for i in range(len_other):
                 result[i] = result[i] + other[i]
-            return result
+            return CustomList(result)
         else:
             raise TypeError(f"unsupported operand type(s) for -")
 
     def __add__(self, other):
-        if isinstance(other, list) or isinstance(other, MyList):
+        if isinstance(other, list) or isinstance(other, CustomList):
             len_self = len(self)
             len_other = len(other)
             result = [0] * max(len_self, len_other)
@@ -38,22 +40,25 @@ class MyList(list):
                 result[i] = result[i] + self[i]
             for i in range(len_other):
                 result[i] = result[i] + other[i]
-            return result
+
+            return CustomList(result)
         else:
-            raise TypeError(f"unsupported operand type(s) for -")
+            raise TypeError(f"unsupported operand type(s) for +")
 
     def __radd__(self, other):
         if isinstance(other, list):
             len_self = len(self)
             len_other = len(other)
             result = [0] * max(len_self, len_other)
+
             for i in range(len_self):
                 result[i] = result[i] + self[i]
             for i in range(len_other):
                 result[i] = result[i] + other[i]
-            return result
+
+            return CustomList(result)
         else:
-            raise TypeError(f"unsupported operand type(s) for -")
+            raise TypeError(f"unsupported operand type(s) for +")
 
     def __lt__(self, other):
         return sum(self) < sum(other)
